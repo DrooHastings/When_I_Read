6db5723ea14668dcd2df1733bfca638a6043b453
Andrew Hastings <droo.hastings@gmail.com>
2017-05-30 10:45:08 -0500

    initial file structure

---
+++ server/server.js
@@ -0,0 +1,26 @@
+//main server file
+const express = require('express');
+const bodyParser = require ('body-parser');
+const path = require ('path');
+const port = process.env.PORT || 5000;
+
+let app = express ();
+
+//Routing Modules here (pre-auth)
+const indexRouter = require('./routes/index');
+
+//bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//static files
+app.use(express.static('public'));
+
+//routes post-auth go here
+app.use('/', indexRouter);
+
+
+//spin up server
+app.listen(port, function() {
+  console.log('up and listening on', port);
+});
