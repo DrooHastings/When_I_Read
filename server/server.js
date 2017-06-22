76e441d62161197f406896f989b6b96b7696a789
Andrew Hastings <droo.hastings@gmail.com>
2017-06-22 10:08:06 -0500

    express added to pkg JSON

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require ('body-parser');
 const path = require ('path');
 const mongoose = require ('mongoose');
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 let app = express ();
 
 //Routing Modules here (pre-auth)
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //db connect
 mongoose.connect('mongodb://heroku_p5pbgnrv:7cfs37trrcql86nipp1rj8hkis@ds135252.mlab.com:35252/heroku_p5pbgnrv');
+// mongoose.connect('mongodb://localhost:27017/whenIread-db');
 mongoose.connection.on('connected', function(){
   console.log('Connected to the DB');
 });
@@ -40,6 +41,6 @@ app.use('/Sounds', soundsRouter);
 app.use('/World', worldRouter);
 
 //spin up server
-app.listen(port, function(req, res) {
+app.listen(port, function() {
   console.log('up and listening on', port);
 });
